d67f556fd0e88645ce5f8a70b0b1c943f97cc682
Angel <angelr4a1@gmail.com>
2020-07-12 02:33:22 -0500

    Sistema de ecs y datos fundamentales
    Se modifica la definicion de las variables ya que se ha supuesto que la entrada del df son todas las columnas fundamentales completas

--- sistema_ecs.py
+++ sistema_ecs.py
@@ -1,7 +1,40 @@
+import numpy as np
+import pandas as pd
+
+#datos
+datos = pd.read_csv('eval_petro(0).csv')
+datos.head()
+#datos['DT'] = np.around(np.array( 189 - (datos['RHOB'] -1)*datos['M']/0.01 ),   decimals =4)
+#datos['N']  = np.around(np.array( (1 - datos['NPHI']) / (datos['RHOB'] - 1)),   decimals = 4)
+#datos['L']  = np.around(np.array( 0.01 * (189-datos['DT']) / (1-datos['NPHI']) ), decimals =4)
+#datos.to_csv('eval_petro(0).csv', index = False)
+#datos.loc[:, datos.columns.str.match('Unnamed: 8')]
+#del datos['Unnamed: 8']    #borrar datos de la columna "Unnamed: 8"
+
+
+M = np.array( 0.01 * (189-datos['DT'])/(datos['RHOB'] - 1) )
+N = np.array( (1 - datos['NPHI']) / (datos['RHOB'] - 1) )
+L = np.array( 0.01 * (189 - datos['DT'])/(1 - datos['NPHI']) )
+datos['M'] = np.around(M, decimals = 4)
+datos['N'] = np.around(N, decimals = 4)
+datos['L'] = np.around(L, decimals = 4)
+#manipulacion de datos como arreglos
+PROF = np.array(datos['PROF'])
+GR   = np.array(datos['GR'])
+LLS  = np.array(datos['LLS'])
+LLD  = np.array(datos['LLD'])
+FR   = np.array(datos['FR'])
+DT   = np.array(datos['DT'])
+NPHI = np.array(datos['NPHI'])
+RHOB = np.array(datos['RHOB'])
+#M    = np.array(datos['M'])
+#N    = np.array(datos['N'])
+#L    = np.array(datos['L'])
 
 """
 ahora toca armar el sistema de ecuaciones y resolverlo para todas las filas
 """
+
 # define matrix A using Numpy arrays
 A = np.matrix([ [189, 43.5, 55.5,   120],
                 [1.0, 0.02, -0.035, 0.33],
